1b00463e3f134acc0827987283173de7311e796c
Anton Ermolenko <aner@fastmail.com>
2017-12-01 22:01:43 +1100

    Fixed PREFIX problem
    I have a variable PREFIX set whenever an installer is running
    NVM detects that variable and refuses to work if it exists.
    I've changed the script so it unsets and resotres this variable
    before and after running nvm installation accordingly

--- installers/node.sh
+++ installers/node.sh
@@ -5,6 +5,9 @@ function has_node {
 }
 
 function get_node {
+	local prfx="$PREFIX"
+	unset PREFIX # because nvm is not compatible with the PREFIX env var
+
 	local curl=$(get_curl)
 	local github_url="https://raw.githubusercontent.com"
 	local nvm_path="creationix/nvm/v0.33.6/install.sh"
@@ -14,6 +17,8 @@ function get_node {
 	. "$HOME/.nvm/nvm.sh"
 	nvm install node
 	nvm use node
+
+	PREFIX="$prfx"
 }
 
 function run_installer_node {
